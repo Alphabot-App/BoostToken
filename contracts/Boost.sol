0a35cf5c0bdcee4e9ad15c82760b28a4bdf1567f
Matt Rivas <matthew.rivas@alphabot.app>
2025-03-24 15:33:24 -0700

    seperate mainnet from l2. mint only on mainnet

--- contracts/Boost.sol
+++ contracts/Boost.sol
@@ -4,17 +4,11 @@ pragma solidity ^0.8.22;
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 import { OFT } from "@layerzerolabs/oft-evm/contracts/OFT.sol";
 
-contract Alphabot is OFT {
+contract Boost is OFT {
     constructor(
         string memory _name,
         string memory _symbol,
         address _lzEndpoint,
-        address _delegate,
-        bool mint,
-        uint256 _initialSupply
-    ) OFT(_name, _symbol, _lzEndpoint, _delegate) Ownable(_delegate) {
-        if (mint) {
-            _mint(msg.sender, _initialSupply);
-        }
-    }
+        address _delegate
+    ) OFT(_name, _symbol, _lzEndpoint, _delegate) Ownable(_delegate) {}
 }
---
+++ contracts/BoostMainnet.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.22;
+
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { OFT } from "@layerzerolabs/oft-evm/contracts/OFT.sol";
+
+contract BoostMainnet is OFT {
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        address _lzEndpoint,
+        address _delegate
+    ) OFT(_name, _symbol, _lzEndpoint, _delegate) Ownable(_delegate) {
+        // mint to deployer
+        _mint(msg.sender, 1_000_000_000 ether);
+    }
+}
