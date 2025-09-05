64d6e04d285300cf57a31c4bd888eb6c61e33cb6
Matt Rivas <matthew.rivas@alphabot.app>
2025-09-05 15:40:11 -0700

    bridgeBoostBscToAbstract

--- utils/safe/transfer.ts
+++ utils/safe/transfer.ts
@@ -200,3 +200,66 @@ export const bridgeBoostBscToMainnet = async (to: string, wad: string) => {
     throw new Error('Failed to create transaction');
   }
 };
+
+export const bridgeBoostBscToAbstract = async (to: string, wad: string) => {
+  const rpc = process.env.RPC_URL_BSC;
+  if (!rpc) throw new Error('RPC_URL_BSC is not defined');
+  const boostAddressBsc = process.env.BOOST_ADDRESS_BSC;
+  if (!boostAddressBsc) throw new Error('BOOST_ADDRESS_BSC is not defined');
+  const hotWalletAddress = process.env.SIGNER_ADDRESS;
+  if (!hotWalletAddress) throw new Error('SIGNER_ADDRESS is not defined');
+  const PK = process.env.GNOSIS_SAFE_PK;
+  if (!PK) throw new Error('GNOSIS_SAFE_PK is not defined');
+
+  const eid = process.env.LAYER2_EID || '30324';
+  if (!eid) throw new Error('LAYER2_EID is not defined');
+  try {
+    const options = Options.newOptions().toBytes();
+
+    const sendParam = {
+      dstEid: eid,
+      to: addressToBytes32(to),
+      amountLD: wad,
+      minAmountLD: wad,
+      extraOptions: options,
+      composeMsg: '0x',
+      oftCmd: '0x',
+    };
+    const boostInterface = new ethers.utils.Interface(boostAbi.abi);
+    const provider = new ethers.providers.JsonRpcProvider(rpc);
+    const contract = new ethers.Contract(
+      boostAddressBsc,
+      boostInterface,
+      provider,
+    );
+    const messageFee = await contract.quoteSend(sendParam, false);
+    const nativeFee = messageFee.nativeFee;
+
+    const data = boostInterface.encodeFunctionData('send', [
+      sendParam,
+      {
+        nativeFee: nativeFee,
+        lzTokenFee: 0,
+      },
+      hotWalletAddress,
+    ]);
+    const tx = {
+      to: boostAddressBsc,
+      value: nativeFee.toString(),
+      data: data,
+      nonce: await provider.getTransactionCount(hotWalletAddress, 'latest'),
+      gasLimit: BigNumber.from(1_000_000),
+      gasPrice: await provider.getGasPrice(),
+      chainId: 56,
+    };
+    // Propose transaction to the service
+    const signer = new ethers.Wallet(PK, provider);
+
+    const res = await signer.sendTransaction(tx);
+
+    return res.hash;
+  } catch (error) {
+    console.error('Error creating transaction:', error);
+    throw new Error('Failed to create transaction');
+  }
+};
